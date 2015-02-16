cd9123c6b144a323fa2f2a128d4413b536b705f8
Sergey Morozov <smorozov@mtt.ru>
2015-02-16 16:42:49 +0300

    few fixes

--- app/api/index.py
+++ app/api/index.py
@@ -3,10 +3,10 @@ from app.lib import auth, log_db
 from flask import g
 
 
-@jsonrpc.method('Log.add(category=str, level=str, msg=str, tags=list) -> list', validate=True)
+@jsonrpc.method('Log.add(category=str, level=str, message=str, timestamp=int) -> list', validate=True)
 @auth.requires_rpc_auth
-def addLog(category, level, msg, tags=[]):
-    log_id = log_db.addLog(g.auth.username, category, level, msg, tags)
+def addLog(category, level, message, timestamp):
+    log_id = log_db.addLog(g.auth.username, category, level, message, timestamp)
     return { "logId": format(log_id) }
 
 
@@ -17,8 +17,28 @@ def getLogAll(category):
     return {"logList": log_db.prepareData(logs) }
 
 
-
 @jsonrpc.method('Log.getCount(category=str) -> list', validate=True)
+@auth.requires_rpc_auth
+def getLogCount(category):
+    log_count = log_db.getLogCount(g.auth.username, category)
+    return { "logCount": int(log_count) }
+
+
+@jsonrpc.method('Log.addCustom(category=str, level=str, msg=str, tags=list) -> list', validate=True)
+@auth.requires_rpc_auth
+def addLog(category, level, msg, tags=[]):
+    log_id = log_db.addLog(g.auth.username, category, level, msg, tags)
+    return { "logId": format(log_id) }
+
+
+@jsonrpc.method('Log.getCustomAll(category=str) -> list', validate=True)
+@auth.requires_rpc_auth
+def getLogAll(category):
+    logs = log_db.getLogAll(g.auth.username, category)
+    return {"logList": log_db.prepareData(logs) }
+
+
+@jsonrpc.method('Log.getCustomCount(category=str) -> list', validate=True)
 @auth.requires_rpc_auth
 def getLogCount(category):
     log_count = log_db.getLogCount(g.auth.username, category)
