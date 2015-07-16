156015888b8da45ab95ae5e8cbd838331158838b
ErgoZ <ergozru@gmail.com>
2015-07-17 01:55:48 +0300

    Update run.py
    Added threading, fixed config

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ from app import app
 from config import PRODUCTION, APP_HOST, APP_PORT
 
 if PRODUCTION:
-    app.run(host=APP_HOST, port=APP_PORT, debug=True)
+    app.run(host=APP_HOST, port=APP_PORT, debug=False, threaded=True, use_reloader=True, reloader_interval=60)
 else:
-    app.run(host=APP_HOST, port=APP_PORT, debug=False)
+    app.run(host=APP_HOST, port=APP_PORT, debug=True, use_reloader=True, reloader_interval=1)
